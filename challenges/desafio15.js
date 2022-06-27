c715666930111aa11d807df447bdb07b6c2b5bcf
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-06-27 20:31:17 -0300

    feat: adicionou desafio15

---
+++ challenges/desafio15.js
@@ -0,0 +1,31 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      avaliacao: 0,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: "bovino" },
+  {
+    $set: {
+      avaliacao: 5,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: "ave" },
+  {
+    $set: {
+      avaliacao: 3,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, avaliacao: 1, _id: 0 },
+);
